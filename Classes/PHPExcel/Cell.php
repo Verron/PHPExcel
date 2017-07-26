1747546fa508b249af7976a606ac3772db8a48a0
James Clark <james.clark@materialink.com>
2017-07-26 10:05:48 -0500

    cast index to integer

--- Classes/PHPExcel/Cell.php
+++ Classes/PHPExcel/Cell.php
@@ -832,7 +832,9 @@ class PHPExcel_Cell
      */
     public static function stringFromColumnIndex($pColumnIndex = 0)
     {
-        //    Using a lookup cache adds a slight memory overhead, but boosts speed
+	$pColumnIndex = (int) $pColumnindex;
+
+	//    Using a lookup cache adds a slight memory overhead, but boosts speed
         //    caching using a static within the method is faster than a class static,
         //        though it's additional memory overhead
         static $_indexCache = array();
